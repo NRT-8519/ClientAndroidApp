a4fd0483d8f1d13907d7493944b446977e08f154
Marko Dujović <dujovicm@outlook.com>
2023-09-03 17:27:07 +0200

    asJson cast

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:ffi';
 
 import 'package:flutter/foundation.dart';
@@ -7,15 +8,71 @@ class User {
   final String? uuid;
   final String firstName, middleName, lastName;
   final String? title;
-  final String? username, password;
+  final String? username;
+  final String? password;
   final String email;
   final String phoneNumber;
   final DateTime dateOfBirth;
-  final Char gender;
+  final String gender; //Dart does not support char type
   final String ssn;
   final DateTime passwordExpiryDate;
   final bool isDisabled;
   final bool isExpired;
 
-  const User(this.uuid, this.firstName, this.middleName, this.lastName, this.title, this.username, this.password, this.email, this.phoneNumber, this.dateOfBirth, this.gender, this.ssn, this.passwordExpiryDate, this.isDisabled, this.isExpired);
+  const User(
+      this.uuid,
+      this.firstName,
+      this.middleName,
+      this.lastName,
+      this.title,
+      this.username,
+      this.password,
+      this.email,
+      this.phoneNumber,
+      this.dateOfBirth,
+      this.gender,
+      this.ssn,
+      this.passwordExpiryDate,
+      this.isDisabled,
+      this.isExpired);
+
+  factory User.fromJson(Map<String, dynamic> parsedJson) {
+    return User(
+      parsedJson["uuid"],
+      parsedJson["firstName"],
+      parsedJson["middleName"],
+      parsedJson["lastName"],
+      parsedJson["title"],
+      parsedJson["username"],
+      parsedJson["password"],
+      parsedJson["email"],
+      parsedJson["phoneNumber"],
+      DateTime.parse(parsedJson["dateOfBirth"]),
+      parsedJson["gender"][0],
+      parsedJson["ssn"],
+      DateTime.parse(parsedJson["passwordExpiryDate"]),
+      parsedJson["isDisabled"],
+      parsedJson["isExpired"]
+    );
+  }
+
+  String asJson() {
+    return json.encode({
+      "uuid": uuid,
+      "firstName": firstName,
+      "middleName": middleName,
+      "lastName": lastName,
+      "title": title,
+      "username": username,
+      "password": password,
+      "email": email,
+      "phoneNumber": phoneNumber,
+      "dateOfBirth": dateOfBirth,
+      "gender": gender,
+      "ssn": ssn,
+      "passwordExpiryDate": passwordExpiryDate,
+      "isDisabled": isDisabled,
+      "isExpired": isExpired
+    });
+  }
 }
